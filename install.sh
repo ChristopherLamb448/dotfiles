4757ef85e9c6e325150e0e1e9e4f28cd4d472663
Christopher Lamb <71055055+ChristopherLamb448@users.noreply.github.com>
2023-02-14 12:35:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ script_dir-$(dirname "$(readlink -f "$0")")
 rm -rf $HOME/.bash_profile
 rm -rf $HOME/.bashrc
 
-ln -s $PWD/bash_profile $HOME/.bash_profile
-ln -s $PWD/bashrc $HOME/.bashrc
+ln -s $HOME/bash_profile $HOME/.bash_profile
+ln -s $HOME/bashrc $HOME/.bashrc
